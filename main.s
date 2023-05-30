9de6328b76decba2e5f08a5d40771bbad98c9e39
Daniel41789 <mariobros4178@gmail.com>
2023-05-29 23:40:43 -0600

    Changes in main.s

--- main.s
+++ main.s
@@ -1,23 +1,64 @@
-/* This program blinks the led embedded in the blue pill board. The led is
- * attached to pin PC13. This pin works as a GPIO, then it must be configured,
- * at assembly level, through the following registers:
- * 1) RCC register,
- * 2) GPIOC_CRL register, 
- * 3) GPIOC_CRH register, and
- * 4) GPIOC_ODR register.
- * 
- * The following code is based on the explanation given in this video:
- * https://www.youtube.com/watch?v=KLWzyhOR3-Y,
- */
-
-
 .thumb              @ Assembles using thumb mode
 .cpu cortex-m3      @ Generates Cortex-M3 instructions
 .syntax unified
 
 .include "ivt.s"
+.include "systick_map.inc"
 .include "gpio_map.inc"
+.include "nvic_reg_map.inc"
+.include "afio_map.inc"
+.include "exti_map.inc"
 .include "rcc_map.inc"
+.extern wait_ms
+.extern Ini_SysTick
+
+speed: 
+        # Prologue
+        push {r7}                               @ backs r7 up
+        sub sp, sp, #4                          @ reserves a 4 bytes function frame
+        add r7, sp, #0
+        # Function Body
+        // Compare
+        bne L1
+        
+L1: 
+
+decrement:
+        # Prologue
+        push {r7, lr}                           @ backs r7 and lr up
+        sub sp, sp, #8                          @ reserves a 8 bytes function frame
+        add r7, sp, #0                          @ updates r7
+        str r0, [r7, #4]                        @ backs function argument
+        # Function body
+        ldr r0, [r7, #4]                        @ r0 <-- function argument
+        subs r0, r0, #1                         @ counter--
+        str r0, [r7, #4]                        @ store counter
+        ldr r0, [r7, #4]                        @ r0 <-- counter
+        # Epilogue
+        adds r7, r7, #8
+        mov sp, r7
+        pop {r7}
+        pop {lr}
+        bx lr
+
+increase:
+        # Prologue
+        push {r7, lr}                           @ backs r7 and lr up
+        sub sp, sp, #8                          @ reserves a 8 bytes function frame
+        add r7, sp, #0                          @ updates r7
+        str r0, [r7, #4]                        @ backs function argument 
+        # Function Body
+        ldr r0, [r7, #4]                        @ r0 <-- function argument
+        adds r0, r0, #1                         @ counter++
+        str r0, [r7, #4]
+        ldr r0, [r7, #4]
+        # Epilogue
+        adds r7, r7, #8
+        mov sp, r7
+        pop {r7}
+        pop {lr}
+        bx lr
+
 
 .section .text
 .align	1
@@ -25,7 +66,11 @@
 .thumb
 .global __main
 __main:
-# Setup
+        # Setup
+        push {r7, lr}
+        sub sp, sp, #16
+        add r7, sp, #0
+
         # enabling clock in port A, B and C
         ldr     r0, =RCC_BASE
         mov     r1, 0x1C                        @ loads 16 in r1 to enable clock in port C (IOPC bit)
@@ -37,35 +82,40 @@ __main:
         str     r1, [r0, GPIOx_CRL_OFFSET]      @ M[GPIOC_CRL] gets 0x44484448
 
         # set pin PB8-PB15 as digital output
-        ldr     r0, =GPIOB_CRH
+        ldr     r0, =GPIOB_BASE
         ldr     r1, =0x33333333                 @ PC13: output push-pull, max speed 50 MHz
         str     r1, [r0, GPIOx_CRH_OFFSET]      @ M[GPIOC_CRH] gets 0x33333333
 
+        ldr r0, =AFIO_BASE
+        mov r1, #0
+        ldr r1, [r0, AFIO_EXTICR1_OFFSET]
+        ldr r0, =EXTI_BASE
+        mov r1, #0
+        str r1, [r0, EXTI_FTST_OFFSET]
+        ldr r1, =0x11
+        str r1, [r0, EXTI_RTST_OFFSET]
+        str r1, [r0, EXTI_IMR_OFFSET]
+        ldr r0, =NVIC_BASE
+        ldr r1, =0x440
+        str r1, [r0, NVIC_ISER0_OFFSET]
+
         # set led status initial value     
-        add     r0, GPIOB_BASE                  @ moves address of GPIOC_ODR register to r0
+        ldr     r0, =GPIOB_BASE                  @ moves address of GPIOC_ODR register to r0
         mov     r1, 0x0
         str     r1, [r0, GPIOx_ODR_OFFSET]
+        mov r1, #0                              @ counter <-- 0
+        str r1, [r7, #4]                        @ Store counter
+
+
 loop:   
-        cmp     r1, 0x0                         @ if r1 equals zero then turn PC13 on
-        bne     L1                              @ else, turns PC13 off
-        mov     r3, 0x0                         @ turns PC13 on
-        b       L2
-L1:     mov     r3, 0x2000                      @ turns PC13 off
-L2:     str     r3, [r0]                        @ M[GPIOC_ODR] gets r1 value
-        # dirty delay
-        ldr     r2, =2666667                    @ r2 gets 2666667
-        b       L3
-L4:     sub     r2, r2, #1
-L3:     cmp     r2, #0
-        bge     L4
-        eor     r1, #1 @ negates LSB of r1
-        b       loop
-
-EXTI0_IRQHandler:
-        # Verification of PA0
-        ldr r0, =GPIOA_BASE
-        ldr r1, [r0, GPIOA_IDR_OFFSET]
-        lsr r1, r1, #0
-        ands r1, r1, #1
-        bne pin
-
+        # Counter 0 or 1
+        bl speed
+        // Compare status counter
+        bne L5
+        ldr r0, [r7, #4]
+        bl increase
+        str r0, [r7, #4]
+        b L6
+L5:
+L6:
+b loop
