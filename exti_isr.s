06f372a15b9cd2dc976d3e5a763e2744ac0bbef5
Daniel41789 <mariobros4178@gmail.com>
2023-05-30 20:12:28 -0600

    added exti_isr.s file

---
+++ exti_isr.s
@@ -0,0 +1,25 @@
+.include "exti_map.inc"
+.extern wait_ms
+
+.cpu cortex-m3      @ Generates Cortex-M3 instructions
+.section .text
+.align	1
+.syntax unified
+.thumb
+.global EXTI0_Handler
+EXTI0_Handler:
+    adds    r10, r10, #1
+    ldr     r0, =EXTI_BASE
+    ldr     r1, [r0, EXTI_PR_OFFSET]
+    orr     r1, r1, 0x40
+    str     r1, [r0, EXTI_PR_OFFSET]
+    bx      lr
+
+.global EXTI4_Handler
+EXTI4_Handler:
+    eor     r11, r11, #1
+    ldr     r0, =EXTI_BASE
+    ldr     r1, [r0, EXTI_PR_OFFSET]
+    orr     r1, r1, 0x400
+    str     r1, [r0, EXTI_PR_OFFSET]
+    bx      lr
