81143848a7541e8a836271b31587cb7761b8d823
Daniel41789 <mariobros4178@gmail.com>
2023-06-05 14:25:27 -0600

    Changes in main

--- main.s
+++ main.s
@@ -2,7 +2,6 @@
 .cpu cortex-m3      @ Generates Cortex-M3 instructions
 .syntax unified
 
-.include "ivt.s"
 .include "systick_map.inc"
 .include "gpio_map.inc"
 .include "nvic_reg_map.inc"
@@ -108,53 +107,54 @@ __main:
         bl Ini_SysTick
         # enabling clock in port A, B and C
         ldr     r0, =RCC_BASE
-        mov     r1, 0x1C                        @ loads 16 in r1 to enable clock in port C (IOPC bit)
-        str     r1, [r0, RCC_APB2ENR_OFFSET]    @ M[RCC_APB2ENR] gets 16
+        mov     r2, 0x1C                        @ loads 16 in r1 to enable clock in port C (IOPC bit)
+        str     r2, [r0, RCC_APB2ENR_OFFSET]    @ M[RCC_APB2ENR] gets 16
 
-        # Set pins PA0 & PA4 as digital input
+        # Set pins PA6 & PA7 as digital input
         ldr     r0, =GPIOA_BASE                 @ moves base address of port A
-        ldr     r1, =0x44484448                 @ this constant signals the reset state
-        str     r1, [r0, GPIOx_CRL_OFFSET]      @ M[GPIOC_CRL] gets 0x44484448
+        ldr     r2, =0x88444444                 @ this constant signals the reset state
+        str     r2, [r0, GPIOx_CRL_OFFSET]      @ M[GPIOC_CRL] gets 0x88444444
 
-        # set pin PB5, PB6 and PB7 as digital output
+        # set pin PB6 and PB7 as digital output
         ldr     r0, =GPIOB_BASE
-        ldr     r1, =0x33344444                 @ PC13: output push-pull, max speed 50 MHz
-        str     r1, [r0, GPIOx_CRL_OFFSET]      @ M[GPIOC_CRH] gets 0x33344444
+        ldr     r2, =0x33444444                 @ PC13: output push-pull, max speed 50 MHz
+        str     r2, [r0, GPIOx_CRL_OFFSET]      @ M[GPIOC_CRH] gets 0x33444444
 
         # set pin PB8-PB14 as digital output
         ldr     r0, =GPIOB_BASE
-        ldr     r1, =0x43333333                 @ PC13: output push-pull, max speed 50 MHz
-        str     r1, [r0, GPIOx_CRH_OFFSET]      @ M[GPIOC_CRH] gets 0x43333333
+        ldr     r2, =0x33333333                 @ PC13: output push-pull, max speed 50 MHz
+        str     r2, [r0, GPIOx_CRH_OFFSET]      @ M[GPIOC_CRH] gets 0x33333333
 
+        #Configuración del mapeo de las interrupcines externas
         ldr r0, =AFIO_BASE
         eor r1, r1
-        str r1, [r0, AFIO_EXTICR1_OFFSET]
         str r1, [r0, AFIO_EXTICR2_OFFSET]
-
+        # Configuración del disparador
         ldr r0, =EXTI_BASE
-        ldr r1, =0x11                           @ 00010001
+        eor r1, r1
         str r1, [r0, EXTI_FTST_OFFSET]
-        ldr r1, =0x0
-        mov r1, #0
+        ldr r1, =(0x3 << 5)
         str r1, [r0, EXTI_RTST_OFFSET]
         str r1, [r0, EXTI_IMR_OFFSET]
         ldr r0, =NVIC_BASE
-        ldr r1, =0x440
+        ldr r1, =(0x1 << 23)                    @ habilitación de EXTI Line[9:5]
         str r1, [r0, NVIC_ISER0_OFFSET]
 
         # set led status initial value     
         ldr     r0, =GPIOB_BASE                  @ moves address of GPIOC_ODR register to r0
         mov     r1, 0x0
         str     r1, [r0, GPIOx_ODR_OFFSET]
+
         mov r1, #0                               @ counter <-- 0
         str r1, [r7, #4]                         @ Store counter
 
-        mov r4, #1                               @ counter status <-- increase
-
+        # Delay <-- 1000ms
         mov r1, #1000                            @ delay <-- 1000ms
         str r1, [r7, #8]                         @ store ms
 
-        mov r5, #1                               @ r10 <-- speed
+        mov r4, #1                               @ set mode status as increment
+
+        mov r5, #1                               @ r5 <-- speed x 1
 
 loop:   
         # Counter 0 or 1
@@ -176,7 +176,7 @@ L6:
         ldr r0, =GPIOB_BASE                     
         ldr r1, [r7, #4]                        @ r1 <-- counter
         mov r2, r1                              @ r2 <-- r1 
-        lsl r2, r2, #5                          @ counter << 8
+        lsl r2, r2, #6                          @ counter << 6
         str r2, [r0, GPIOx_ODR_OFFSET]
         ldr r0, [r7, #8]                        @ r0 <-- ms
         bl wait_ms                              @ jump to wait_ms function
